5a8984eef623fed2e9c9741bac6e3cb2fd28fef1
Guilherme Santos <guiss.devs@gmail.com>
2023-08-03 21:24:46 -0300

    Adicionando Header e Home

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import './header.css'
+
+const Header = () => {
+    return (
+        <header>
+            <div className='cabecalho'>
+                <h4>guilherme-santos</h4>
+                <h4>_hello</h4>
+                <h4>_sobre-mim</h4>
+                <h4>_projetos</h4>
+                <h4>_contato</h4>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Home/index.js
@@ -0,0 +1,22 @@
+import './home.css'
+
+const Home = () => {
+    return (
+        <div className='presentation'>
+            <div>
+                <h3>Olá a todos. Me chamo</h3>
+                <h1>Guilherme Santos</h1>
+                <h2>&gt; Desenvolvedor Front-end</h2>
+
+                <br />
+                <br />
+
+                <p>// seja bem vindo ao meu portifólio</p>
+                <p>// você também pode conferir meu perfil do github</p>
+                <p className='link'><span className='const-word'>const</span> <span className='github-link'>githubLink</span> = <a href='https://github.com/Guissdev' target='_blank'><span className='link-name'>"https://github.com/Guissdev"</span></a></p>
+            </div>
+        </div>
+    );
+}
+
+export default Home
