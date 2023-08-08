9e12e133ef9b44f936751aa5b1020b23ffa0a5f2
Guilherme Santos <guiss.devs@gmail.com>
2023-08-08 16:32:54 -0300

    Ajustando responsividade

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,35 @@
 import { BiLogoLinkedin} from 'react-icons/bi'
 import { FaWhatsapp } from 'react-icons/fa'
 import { AiFillGithub } from 'react-icons/ai'
+import React, { useState, useEffect } from 'react';
 import './footer.css'
 
 const Footer = () => {
+
+    const [isFooterVisible, setIsFooterVisible] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollY = window.scrollY;
+            const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
+            const isScrollable = maxScroll > 0;
+
+            if (!isScrollable) {
+                setIsFooterVisible(true);
+            } else {
+                setIsFooterVisible(scrollY >= maxScroll);
+            }
+        };
+        
+        handleScroll();
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+    
+
     return (
         <footer>
-            <div className='rodape'>
+             <div className={isFooterVisible ? 'visible-footer' : 'hidden-footer'}>
                 <h4>encontre me:</h4>
                 
                 <BiLogoLinkedin 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,22 @@
-import './header.css'
+import './header.css';
+import React, { useState, useEffect } from 'react';
 
 const Header = () => {
+    const [isHeaderVisible, setIsHeaderVisible] = useState(true);
+
+    useEffect(() => {
+
+        const handleScroll = () => {
+            setIsHeaderVisible(window.pageYOffset === 0);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
         <header>
-            <div className='cabecalho'>
+            <div className={isHeaderVisible ? 'visible' : 'hidden'}>
                 <h4 className='name'>guilherme-santos</h4>
                 <h4 className='navi'>_hello</h4>
                 <h4 className='navi'>_sobre-mim</h4>
@@ -14,4 +27,4 @@ const Header = () => {
     )
 }
 
-export default Header
+export default Header;
