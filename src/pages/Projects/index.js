fbea00826a2e9a016a4652935d0e1a1751dc3aa6
Guilherme Santos <guiss.devs@gmail.com>
2023-08-29 16:48:40 -0300

    Adicionando elementos

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -2,9 +2,31 @@ import './projects.css'
 
 const Projects = () => {
     return (
-        <div>
-           
+        <div className='projects'>
+            <div className='card'>
+                <img className='image' src='./images/image.png'/>
+                <div>
+                    <p>//Este é um projeto</p>
+                    <bottom>Ver projeto</bottom>
+                </div>
+            </div>
+            <div className='card'>
+                <img className='image' src='./images/image.png'/>
+                <div>
+                    <p>//Este é um projeto</p>
+                    <bottom>Ver projeto</bottom>
+                </div>
+            </div>
+            <div className='card'>
+                <img className='image' src='./images/image.png'/>
+                <div>
+                    <p>//Este é um projeto</p>
+                    <bottom>Ver projeto</bottom>
+                </div>
+            </div>
         </div>
+        
+        
     )
 }
 
